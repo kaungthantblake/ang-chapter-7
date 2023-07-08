cd4516d6582482a24bc2df6ad8e9039d41f876af
kaungthantblake <shido1812@gmail.com>
2023-07-08 14:57:31 +0630

    first commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chapter-7';
+
+  constructor() {}
+
+  username: string = '';
+  password: string = '';
+
+  Check() {
+    console.log(
+      'username' + ' ' + this.username + ' ' + ' Password ' + this.password
+    );
+  }
+  Enter() {
+    if (this.username === 'K' && this.password === '123456') {
+      alert('match');
+    } else {
+      alert('not match');
+    }
+  }
+
+  ngOnInit(): void {}
 }
